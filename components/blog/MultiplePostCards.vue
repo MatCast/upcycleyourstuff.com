d1d13208d8ecc665b00774f292fa1eaa084ad1bf
MatCast <32680270+MatCast@users.noreply.github.com>
2021-11-19 13:22:03 +0100

    Add. basic post feed.

---
+++ components/blog/MultiplePostCards.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="article in articles" :key="article.slug">
+          <BlogPostCard :article-title="article.title" :article-image="article.image"/>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { articles: null }
+  },
+  async fetch() {
+    this.articles = await this.$content('', { deep: true }).fetch()
+  },
+}
+</script>
+
+<style></style>
---
+++ components/blog/PostCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <h3>{{articleTitle}}</h3>
+      <img :src="`/${articleImage}`" :alt="articleTitle">
+      <p>{{articleDescription}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    articleTitle: {
+      type: String,
+      required: true,
+    },
+    articleImage: {
+      type: String,
+      required: true,
+    },
+    articleDescription: {
+      type: String,
+      required: false,
+      default:'',
+    },
+  },
+}
+</script>
+
+<style></style>
