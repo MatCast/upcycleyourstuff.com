f566b9988aacd414fdedcc24c942f765669f64e3
MatCast <32680270+MatCast@users.noreply.github.com>
2021-11-23 20:53:46 +0100

    fix justify flex center.

--- components/blog/PostCardContainer.vue
+++ components/blog/PostCardContainer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col">
     <div class="flex md:flex-col p-5 items-center">
-      <div class="w-full m-5">
+      <div class="w-full m-5 flex justify-center">
         <NuxtLink :to="`/blog/${articleSlug}`">
           <img
             width="400"
