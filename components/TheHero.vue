7e96be26357eefc1315b31775f91becbd7d15fc8
MatCast <32680270+MatCast@users.noreply.github.com>
2021-11-23 22:21:24 +0100

    Add basic Hero.

---
+++ components/TheHero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="hero" class="hero h-80 w-full flex items-center">
+    <h1 class="text-8xl font-bold text-center uppercase text-white">Upycle your Clothes</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.hero {
+  background-image: url('/fili.jpg');
+  background-size: cover;
+}
+</style>
