19abe0a5c8221a435adfd2c1668bac35f2236d85
MatCast <32680270+MatCast@users.noreply.github.com>
2021-11-25 20:58:19 +0100

    Imrpove header style.

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
-  <header>
-    <nav class="main-header">
-      <ul class="inline-flex w-full space-x-8 justify-center p-5 uppercase bg-white">
+  <header class="top-0 z-50 w-full bg-secondary">
+    <nav class="main-header flex justify-center">
+      <ul class="inline-flex w-full space-x-8 justify-center p-5 uppercase font-bold bg-secondary border-black border-b-2 shadow-xl">
         <li v-for="(item, key) of items" :key="key" :to="item.to">
           <NuxtLink :to="item.to">
             {{ item.title }}
@@ -35,7 +35,7 @@ export default {
 
 <style>
 nav.main-header a.nuxt-link-exact-active {
-  @apply border-b-2 border-primary;
+  @apply border-b-2 border-accent;
   /* font-weight: bold; */
 }
 </style>
