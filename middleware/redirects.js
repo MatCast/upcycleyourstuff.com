e3d7dc75c6bbdffac46dcc02a6860e95df60c05f
MatCast <32680270+MatCast@users.noreply.github.com>
2022-09-08 14:34:31 +0200

    fix redirects.

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -1,9 +1,8 @@
 const redirects = require('../data/redirects.json') // update to your file path
 
-export default function (req, redirect, next) {
+export default function ({route, redirect}) {
   // find the redirect if it exists where the from === the requested url
-  const toRedirect = redirects.find(({ from }) => from === req.url)
-
+  const toRedirect = redirects.find(({ from }) => from === route.name)
   // If it exists, redirect the page with a 301 response else carry on
   if (toRedirect) {
     return redirect(301, toRedirect.to)
