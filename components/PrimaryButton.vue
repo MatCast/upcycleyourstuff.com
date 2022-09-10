e067222e94b99f0225f3435c78aab2752ea16008
MatCast <32680270+MatCast@users.noreply.github.com>
2022-09-10 21:12:46 +0200

    add primary button.

---
+++ components/PrimaryButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="border-2 border-black px-4 py-2 bg-secondary text-black uppercase">
+    {{ buttonText }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: {
+      required: true,
+      type: String,
+    },
+  },
+}
+</script>
