acbd9a332bd5520a8854d9d841b7616dc8781e5b
MatCast <32680270+MatCast@users.noreply.github.com>
2022-09-10 18:19:49 +0200

    add privacy policy.

---
+++ pages/privacy.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <nuxt-content class="max-w-2xl" :document="privacyPolicy" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    let privacyPolicy
+    try {
+      privacyPolicy = await $content('privacy_policy', 'privacy').fetch()
+    } catch (e) {
+      return error({ statusCode: 404, message: 'Page not found' })
+    }
+    return { privacyPolicy }
+  },
+  data() {
+    return {}
+  },
+  head() {
+    return {
+      title: 'Privacy Policy',
+    }
+  },
+}
+</script>
+
+<style scoped>
+* >>> h1 {
+  @apply text-4xl mb-5 pt-7 font-semibold capitalize;
+}
+
+* >>> h2 {
+  @apply text-3xl mb-5 pt-7 font-semibold capitalize;
+}
+
+* >>> p {
+  @apply mb-5 text-sm;
+}
+</style>
